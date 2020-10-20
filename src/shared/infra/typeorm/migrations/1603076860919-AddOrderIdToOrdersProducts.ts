c5041dd6f5af67a4be583faec9eead6c0360dce7
Rudinei Luiz Hoehn <rudineilh@gmail.com>
2020-10-19 22:17:39 -0300

    Desafio relations typeORM

---
+++ src/shared/infra/typeorm/migrations/1603076860919-AddOrderIdToOrdersProducts.ts
@@ -0,0 +1,38 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddOrderIdToOrdersProducts1603076860919
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders_products',
+      new TableColumn({
+        name: 'order_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'orderproductsorder',
+        columnNames: ['order_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'orders',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders_products', 'orderproductsorder');
+
+    await queryRunner.dropColumn('orders_products', 'order_id');
+  }
+}
